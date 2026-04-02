8272bd360c44da8d55604799fb34ad0bd35cc8ea
mat-zyd <matthias.zydorek@stud.uni-goettingen.de>
2026-04-02 15:18:11 +0200

    update on taxonstandardization

--- R/RMMS_documentation.R
+++ R/RMMS_documentation.R
@@ -11,3 +11,5 @@ rmm$authorship$organization <- "University of Göttingen"
 rmm$dataoccurence$taxon <- df_red_list_forest_species$taxon_std
 rmm$data$occurence$dataType <- "presence only"
 rmm$dataoccurence$source <- "European Vegetation Archive (EVA)"
+rmm$data$occurrence$taxonStandardizationMethod  <- paste("rgnparser", "v1.14.2", "POWO via KewR")
+rmm$data$occurrence$taxonStandardizationVersion <- "2026-04-02"
